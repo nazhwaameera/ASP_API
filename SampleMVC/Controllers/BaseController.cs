112792fa02aeccfd528d28b92ef1370f28bbcf18
Nazhwa Ameera Hardy <nazhwaameera@gmail.com>
2024-03-13 16:03:52 +0700

    Initial commit

---
+++ SampleMVC/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyWebFormApp.BLL.DTOs;
+
+namespace SampleMVC.Controllers
+{
+    public class BaseController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (HttpContext.Session.GetString("user") != null)
+            {
+                var userDtoSerialize = HttpContext.Session.GetString("user");
+                var userDto = JsonSerializer.Deserialize<UserDTO>(userDtoSerialize);
+                ViewBag.UserName = $"{userDto.FirstName} {userDto.LastName}";
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
--- SampleMVC/Controllers/UsersController.cs
+++ SampleMVC/Controllers/UsersController.cs
@@ -1,17 +1,129 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyWebFormApp.BLL.DTOs;
+using MyWebFormApp.BLL.Interfaces;
+using System.Text.Json;
 
 namespace SampleMVC.Controllers
 {
     public class UsersController : Controller
     {
+        private readonly IUserBLL _userBLL;
+        private readonly IRoleBLL _roleBLL;
+
+        public UsersController(IUserBLL userBLL, IRoleBLL roleBLL)
+        {
+            _userBLL = userBLL;
+            _roleBLL = roleBLL;
+        }
+
+
         public IActionResult Index()
+        {
+            var usersWithRoles = _userBLL.GetAllWithRoles();
+            return View(usersWithRoles);
+        }
+
+        public IActionResult Login()
+        {
+            if (TempData["Message"] != null)
+            {
+                ViewBag.Message = TempData["Message"];
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginDTO loginDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            try
+            {
+                var userDto = _userBLL.LoginMVC(loginDTO);
+                //simpan username ke session
+                var userDtoSerialize = JsonSerializer.Serialize(userDto);
+                HttpContext.Session.SetString("user", userDtoSerialize);
+
+                TempData["Message"] = "Welcome " + userDto.Username;
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = @"<div class='alert alert-danger'><strong>Error!&nbsp;</strong>" + ex.Message + "</div>";
+                return View();
+            }
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("user");
+            return RedirectToAction("Login");
+        }
+
+        //register user baru
+        public IActionResult Register()
         {
             return View();
         }
 
-        public IActionResult Create()
+        [HttpPost]
+        public IActionResult Register(UserCreateDTO userCreateDto)
         {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            try
+            {
+                _userBLL.Insert(userCreateDto);
+                ViewBag.Message = @"<div class='alert alert-success'><strong>Success!&nbsp;</strong>Registration process successfully !</div>";
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = @"<div class='alert alert-danger'><strong>Error!&nbsp;</strong>" + ex.Message + "</div>";
+            }
+
             return View();
+        }
+
+        public IActionResult Profile()
+        {
+            //var userWithRoles = _userBLL.GetUserWithRoles("ekurniawan");
+            var usersWithRoles = _userBLL.GetAllWithRoles();
+            return new JsonResult(usersWithRoles);
+        }
+
+        public IActionResult Edit(string username)
+        {
+            var editProfile = _userBLL.GetByUsername(username);
+            // Retrieve the data from your database or any other source
+            var dropdownData = _roleBLL.GetAllRoles();
+
+            // Populate ViewBag with the dropdown data
+            ViewBag.DropdownData = dropdownData;
+            return View(editProfile);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(string username, int RoleID)
+        {
+            try
+            {
+                _roleBLL.AddUserToRole(username, RoleID);
+                TempData["Message"] = "User " + username + " has been added to role";
+            }
+            catch (Exception ex)
+            {
+                TempData["Message"] = ex.Message;
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
